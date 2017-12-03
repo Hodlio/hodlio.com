8d4f81ca213b491136fff306ba602fc08bf73389
Matthew Hetherington <matt@hetherington.tv>
2017-12-03 19:49:34 +0000

    Merge server and client

--- Server/Server.js
+++
@@ -1,5 +0,0 @@
-const GdaxClient = require('./ExchangeApiClients/GdaxClient');
-
-var Gdax = new GdaxClient();
-
-Gdax.connectToPriceFeed();
---
+++ src/server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const GdaxClient = require('./ExchangeApiClients/GdaxClient');
+// var Gdax = new GdaxClient();
+//
+// Gdax.connectToPriceFeed();
+
+app.use(express.static('build'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + 'build/index.html'));
+});
+
+app.listen(8083, () => console.log('Server listening on port 8083.'));
