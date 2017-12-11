ce531fc1b1aea0015b85b656f0cbd6a29a45c355
Matthew Hetherington <matt@hetherington.tv>
2017-12-11 14:09:48 +0000

    Hookup login form

--- src/dataLayer/auth.js
+++ src/dataLayer/auth.js
@@ -8,5 +8,15 @@ module.exports = {
             }),
             body: JSON.stringify({ email, password })
         })
+    },
+    login: function login(email, password) {
+        fetch('/api/login', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({ email, password })
+        })
     }
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,32 +6,51 @@ import InputContainerWithLabel from '../components/Common/InputContainerWithLabe
 import Button from '../components/Common/Button';
 import { Link } from 'react-router-dom';
 import waves from '../waves@2x.png';
+import { login } from '../dataLayer/auth';
 
 class Login extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+    }
+
+    updateEmail = (e) => this.setState({ email: e.target.value });
+    updatePassword = (e) => this.setState({ password: e.target.value });
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        login(this.state.email, this.state.password);
+    };
+
     render() {
         return (
-           <div className="authStyles">
+           <form className="authStyles" onSubmit={this.handleSubmit}>
                <Nav/>
                <img src={waves} className="authStyles__wavesTop" alt="Hodlio" />
                <h1 className="authStyles__pageHeading">Login to your account.</h1>
                <div className="authStyles__form">
                    <InputContainerWithLabel label="Email">
-                       <Input />
+                       <Input value={this.state.email} onChange={this.updateEmail} />
                    </InputContainerWithLabel>
 
                    <InputContainerWithLabel label="Password">
-                       <Input />
+                       <Input value={this.state.password} onChange={this.updatePassword} type="password" />
                    </InputContainerWithLabel>
 
                    <div className="authStyles__formFooter">
-                       <Button>Login</Button>
+                       <Button type="submit">Login</Button>
                    </div>
 
                    <Link to="/create-account" className="authStyles__link">Don't have an account yet?</Link>
 
                </div>
                <img src={waves} className="authStyles__waves" alt="Hodlio" />
-           </div>
+           </form>
         );
     }
 }
