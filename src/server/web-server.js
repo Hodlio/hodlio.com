10139c1c45336190dc8ce72aa49a4a675bfbdbca
Matthew Hetherington <matt@hetherington.tv>
2017-12-13 20:47:20 +0000

    Fix missing bracket

--- src/server/web-server.js
+++ src/server/web-server.js
@@ -25,7 +25,7 @@ app.all("/api/*", function(req, res) {
     apiProxy.web(req, res, { target: 'http://localhost:8084' });
 });
 
-app.use(express.static(path.join(__dirname, '../../build'));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../build/index.html'));
